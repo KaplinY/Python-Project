4835f4cf512514da2ea24052388f1d1052ca1d1d
Yaroslav <kapliny@mcmaster.ca>
2023-03-10 13:38:38 -0500

    added main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+from typing import Union
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Perc(BaseModel):
+    value: float
+    percent: float
+
+
+app = FastAPI()
+
+
+@app.post("/calculate_percents/")
+async def create_item(item: Perc):
+    if item.percent < 0:
+        return {"message": "try positive value"}
+    else:
+        sum = item.value + item.percent*item.value/100
+        sub = item.value - item.percent*item.value/100
+        per = item.percent*item.value/100
+        item_dict_result = [{"added":sum, "subtracted":sub, "percent":per }]
+    return item_dict_result

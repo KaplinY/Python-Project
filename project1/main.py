c5eb84d8b7ae6455ff1d8fc962f63ab570ec97ab
Yaroslav <kapliny@mcmaster.ca>
2023-03-17 16:48:15 -0400

    database added

--- project1/main.py
+++ project1/main.py
@@ -1,7 +1,9 @@
 from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
-
+import psycopg
+from datetime import date
+from datetime import datetime
 
 class Perc(BaseModel):
     value: float
@@ -21,3 +23,24 @@ async def create_item(item: Perc):
         per = item.percent*item.value/100
         item_dict_result = [{"added":sum, "subtracted":sub, "percent":per }]
     return item_dict_result
+
+now = datetime.now()
+
+with psycopg.connect("dbname=percents user=postgres") as conn:
+
+    # Open a cursor to perform database operations
+    with conn.cursor() as cur:
+
+        # Execute a command: this creates a new table
+        cur.execute("""
+            CREATE TABLE [IF NOT EXISTS] percents_data (
+                added FLOAT (50) NOT NULL
+                subtracted FLOAT (50) NOT NULL
+                percent FLOAT (50) NOT NULL
+                time TIMESTAMP
+            """)
+        cur.execute("""
+            INSERT INTO percernts_data(added, subtracted, percent, time)
+            VALUES (sum, sub, per, now)
+        """)
+        

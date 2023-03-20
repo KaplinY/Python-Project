afe5dbb96fdabd5d2f3b7468e4ac6cb7227e962c
Yaroslav <kapliny@mcmaster.ca>
2023-03-20 12:35:06 -0400

    main modified

--- project1/main.py
+++ project1/main.py
@@ -12,6 +12,21 @@ class Perc(BaseModel):
 
 app = FastAPI()
 
+@app.on_event("startup")
+async def startup_event():
+    with psycopg.connect("dbname=percents user=postgres password=postgres") as conn:
+
+    # Open a cursor to perform database operations
+        with conn.cursor() as cur:
+
+        # Execute a command: this creates a new table
+            cur.execute("""
+                CREATE TABLE IF NOT EXISTS percents_data (
+                    added FLOAT (50) NOT NULL,
+                    subtracted FLOAT (50) NOT NULL,
+                    percent FLOAT (50) NOT NULL,
+                    time TIMESTAMP)
+                """)
 
 @app.post("/calculate_percents")
 async def create_item(item: Perc):
@@ -22,25 +37,19 @@ async def create_item(item: Perc):
         sub = item.value - item.percent*item.value/100
         per = item.percent*item.value/100
         item_dict_result = [{"added":sum, "subtracted":sub, "percent":per }]
+
+    now = datetime.now()
+
+    with psycopg.connect("dbname=percents user=postgres password=postgres") as conn:
+
+        with conn.cursor() as cur:
+            cur.execute(
+            "INSERT INTO percents_data (added, subtracted, percent, time) VALUES (%s, %s, %s, %s)",
+            (item_dict_result["added"], item_dict_result["subtracted"], item_dict_result["percent"], now))
+
+
     return item_dict_result
 
-now = datetime.now()
 
-with psycopg.connect("dbname=percents user=postgres") as conn:
 
-    # Open a cursor to perform database operations
-    with conn.cursor() as cur:
-
-        # Execute a command: this creates a new table
-        cur.execute("""
-            CREATE TABLE [IF NOT EXISTS] percents_data (
-                added FLOAT (50) NOT NULL
-                subtracted FLOAT (50) NOT NULL
-                percent FLOAT (50) NOT NULL
-                time TIMESTAMP
-            """)
-        cur.execute("""
-            INSERT INTO percernts_data(added, subtracted, percent, time)
-            VALUES (item_dict_result["added"], item_dict_result["subtacted"], item_dict_result["percent"], now)
-        """)
         

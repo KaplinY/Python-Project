1e9002393bda936615e35bc01e4af1e4d3caedfe
Yaroslav <kapliny@mcmaster.ca>
2023-07-04 15:18:52 -0700

    pytest modified

--- tests/test_main.py
+++ tests/test_main.py
@@ -12,7 +12,7 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 async def test_add_user(test_app: AsyncClient):
     response = await test_app.post(
         "/users/add_user",
-        json={"username":"user123", "password":"user123!"},
+        json={"username":"user123", "password":"user123!", "email":"user123@y.ru"},
     )
     assert response.status_code == 200
     assert response.json() == None
@@ -31,7 +31,7 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
 async def test_authenticate_user(test_app: AsyncClient):
     response = await test_app.post(
         "/users/authenticate_user",
-        json={"username":"user", "password":"user!"},
+        json={"username":"user", "password":"user!", "email":"user123@y.ru"},
     )
     assert response.status_code == 200
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
